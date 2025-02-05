561f687feaecade7f059171f33f87df87120f40a
toluhikay <kayodesunday29@gmail.com>
2025-02-05 17:36:13 +0100

    forat push

--- src/components/About/OurCompany/ourCompany.jsx
+++ src/components/About/OurCompany/ourCompany.jsx
@@ -4,76 +4,79 @@ import { GiThink } from "react-icons/gi";
 import { MdCleanHands, MdOutlineSmokeFree } from "react-icons/md";
 
 const OurCompany = () => {
-	const why = [
-		{
-			id: 1,
-			icon: <FaHandshake className='text-3xl font-bold text-white' />,
-			h1: "Integrity",
-			p: `We uphold unwavering honesty and transparency in everything we do.`,
-		},
-		{
-			id: 2,
-			icon: <GiThink className='text-3xl font-bold text-white' />,
-			h1: "Innovation",
-			p: `We continuously seek creative solutions to drive progress and excellence.`,
-		},
-		{
-			id: 3,
-			icon: <MdCleanHands className='text-3xl font-bold text-white' />,
-			h1: "Ethical and Social Responsibility",
-			p: `We prioritize ethical conduct and contribute positively to society.`,
-		},
-		{
-			id: 4,
-			icon: <MdOutlineSmokeFree className='text-3xl font-bold text-white' />,
-			h1: "Environmental Stewardship",
-			p: `We are dedicated to preserving and nurturing our planet's well-being.`,
-		},
-		{
-			id: 5,
-			icon: <FaSmileWink className='text-3xl font-bold text-white' />,
-			h1: "Empathy",
-			p: `We empathize with our colleagues, clients, and communities, fostering understanding and compassion in all our interactions.`,
-		},
-		{
-			id: 6,
-			icon: <FaPeopleCarry className='text-3xl font-bold text-white' />,
-			h1: "Team Spirit",
-			p: `We value collaboration and foster a sense of unity, recognizing that our collective strength is greater than our individual efforts.`,
-		},
-	];
-	return (
-		<div className='bg-secondary text-white lg:px-24 md:px-12 px-6 lg:py-12 md:py-4 py-3 flex flex-col justify-center items-center text-center'>
-			<div data-aos='fade-left' className='md:w-[70%] w-full mb-12'>
-				<p className='lg:text-2xl font-bold text-xl mb-3 text-primary'>Our Company</p>
-				<p className='text-base'>Viridi Consults Limited is a limited liability company incorporated under the Companies and Allied Matters Act 1990. Our commitment to excellence is not just a promise; it's a legal commitment. This status reflects our unwavering dedication to providing you with the highest quality environmental consulting services. we are your trusted ally in navigating the intricate and ever-evolving landscape of environmental stewardship. </p>
-			</div>
-			<div className='md:w-[80%] w-full flex flex-col justify-between h-auto'>
-				<div data-aos='fade-right' className='mb-12'>
-					<p className='lg:text-2xl font-bold text-xl text-green-500 mb-3'>Our Mission</p>
-					<p className='text-sm'>To provide our clients with excellent and satisfactory services, so that they can easily recommend us to others.</p>
-				</div>
-				<div>
-					<p className='lg:text-2xl font-bold text-xl mb-3 text-green-500'>Our Core Values</p>
-					<div className='flex flex-wrap w-full justify-between mt-6'>
-						{why.map((items, index) => {
-							return (
-								<div data-aos={index % 2 === 0 ? "fade-right" : "fade-left"} key={items.id} className='md:w-[48%] mb-12 items-center  w-full flex flex-col text-center'>
-									<div className='mr-3'>
-										<div className='bg-primary rounded-full xl:p-3 p-1 h-auto w-auto'>{items.icon}</div>
-									</div>
-									<div>
-										<p className='xl:text-2xl md:text-xl text-base font-bold text-white'>{items.h1}</p>
-										<p className='text-sm leading-loose'>{items.p}</p>
-									</div>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const why = [
+    {
+      id: 1,
+      icon: <FaHandshake className="text-3xl font-bold text-white" />,
+      h1: "Integrity",
+      p: `We uphold unwavering honesty and transparency in everything we do.`,
+    },
+    {
+      id: 2,
+      icon: <GiThink className="text-3xl font-bold text-white" />,
+      h1: "Innovation",
+      p: `We continuously seek creative solutions to drive progress and excellence.`,
+    },
+    {
+      id: 3,
+      icon: <MdCleanHands className="text-3xl font-bold text-white" />,
+      h1: "Ethical and Social Responsibility",
+      p: `We prioritize ethical conduct and contribute positively to society.`,
+    },
+    {
+      id: 4,
+      icon: <MdOutlineSmokeFree className="text-3xl font-bold text-white" />,
+      h1: "Environmental Stewardship",
+      p: `We are dedicated to preserving and nurturing our planet's well-being.`,
+    },
+    {
+      id: 5,
+      icon: <FaSmileWink className="text-3xl font-bold text-white" />,
+      h1: "Empathy",
+      p: `We empathize with our colleagues, clients, and communities, fostering understanding and compassion in all our interactions.`,
+    },
+    {
+      id: 6,
+      icon: <FaPeopleCarry className="text-3xl font-bold text-white" />,
+      h1: "Team Spirit",
+      p: `We value collaboration and foster a sense of unity, recognizing that our collective strength is greater than our individual efforts.`,
+    },
+  ];
+  return (
+    <div className="bg-secondary text-white lg:px-24 md:px-12 px-6 lg:py-12 md:py-4 py-3 flex flex-col justify-center items-center text-center">
+      <div data-aos="fade-left" className="md:w-[70%] w-full mb-12">
+        <p className="lg:text-2xl font-bold text-xl mb-3 text-primary">Our Company</p>
+        <p className="text-base">
+          Viridi Consults Limited is a limited liability company incorporated under the Companies and Allied Matters Act 1990. Our commitment to excellence is not just a promise; it's a legal commitment. This status reflects our unwavering dedication to providing you with the highest quality environmental consulting services. we are your trusted ally in navigating the intricate and ever-evolving
+          landscape of environmental stewardship.{" "}
+        </p>
+      </div>
+      <div className="md:w-[80%] w-full flex flex-col justify-between h-auto">
+        <div data-aos="fade-right" className="mb-12">
+          <p className="lg:text-2xl font-bold text-xl text-green-500 mb-3">Our Mission</p>
+          <p className="text-sm">To provide our clients with excellent and satisfactory services, so that they can easily recommend us to others.</p>
+        </div>
+        <div>
+          <p className="lg:text-2xl font-bold text-xl mb-3 text-green-500">Our Core Values</p>
+          <div className="flex flex-wrap w-full justify-between mt-6">
+            {why.map((items, index) => {
+              return (
+                <div data-aos={index % 2 === 0 ? "fade-right" : "fade-left"} key={items.id} className="md:w-[48%] mb-12 items-center  w-full flex flex-col text-center">
+                  <div className="mr-3">
+                    <div className="bg-primary rounded-full xl:p-3 p-1 h-auto w-auto">{items.icon}</div>
+                  </div>
+                  <div>
+                    <p className="xl:text-2xl md:text-xl text-base font-bold text-white">{items.h1}</p>
+                    <p className="text-sm leading-loose">{items.p}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurCompany;
--- src/components/NavigationComponent/navigation.jsx
+++ src/components/NavigationComponent/navigation.jsx
@@ -7,109 +7,109 @@ import Logo from "assets/NewAssets/ViridiLogo2.png";
 import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 const Navigation = () => {
-	const [navOpen, setNavOpen] = useState(true);
+  const [navOpen, setNavOpen] = useState(true);
 
-	const handleNavOpen = () => {
-		setNavOpen(!navOpen);
-	};
+  const handleNavOpen = () => {
+    setNavOpen(!navOpen);
+  };
 
-	const socials = [
-		{
-			id: 1,
-			name: "linkedin",
-			logo: <FaLinkedin />,
-			link: "https://www.linkedin.com/company/viridi-consults-limited/",
-		},
-		{
-			id: 2,
-			name: "whatsapp",
-			logo: <FaWhatsapp />,
-			link: "https://api.whatsapp.com/send?phone=2347043837916&text=Welcome%20to%20Viridi%20Consults%20Limited.%20Your%20trusted%20partner%20in%20Environmental%20%20Consulting.%20%20How%20may%20we%20be%20of%20help%20to%20you,%20today?",
-		},
-		{
-			id: 3,
-			name: "instagram",
-			logo: <FaInstagram />,
-			link: "https://www.instagram.com/viridiconsultslimited/",
-		},
-	];
-	const links = [
-		{ id: 1, link: "home", to: "/" },
-		{ id: 2, link: "about us", to: "/about" },
-		{ id: 3, link: "services", to: "/services" },
-		{ id: 4, link: "contact", to: "/contact" },
-	];
+  const socials = [
+    {
+      id: 1,
+      name: "linkedin",
+      logo: <FaLinkedin />,
+      link: "https://www.linkedin.com/company/viridi-consults-limited/",
+    },
+    {
+      id: 2,
+      name: "whatsapp",
+      logo: <FaWhatsapp />,
+      link: "https://api.whatsapp.com/send?phone=2347043837916&text=Welcome%20to%20Viridi%20Consults%20Limited.%20Your%20trusted%20partner%20in%20Environmental%20%20Consulting.%20%20How%20may%20we%20be%20of%20help%20to%20you,%20today?",
+    },
+    {
+      id: 3,
+      name: "instagram",
+      logo: <FaInstagram />,
+      link: "https://www.instagram.com/viridiconsultslimited/",
+    },
+  ];
+  const links = [
+    { id: 1, link: "home", to: "/" },
+    { id: 2, link: "about us", to: "/about" },
+    { id: 3, link: "services", to: "/services" },
+    { id: 4, link: "contact", to: "/contact" },
+  ];
 
-	return (
-		<Fragment>
-			<div className={`lg:px-24 md:px-12 px-6 bg-secondary z-[999999] text-white py-1 ${!navOpen ? "hidden" : "flex"} justify-between items-center`}>
-				<div className='flex flex-col md:flex-row'>
-					<p className='flex items-center justify-start md:justify-center text-xs mr-2'>
-						<FiPhoneCall className='mr-1' /> (+234) 7043837916
-					</p>
-					<p className='flex items-center justify-start md:justify-center text-xs'>
-						<SiMinutemailer className='mr-1' /> info@viridiconsultsltd.com.ng
-					</p>
-				</div>
-				<div className='flex'>
-					{socials.map((item) => {
-						return (
-							<Fragment key={item.id}>
-								<a href={item.link} key={item.id} target='_blank' rel='noreferrer' className='ml-3'>
-									{item.logo}
-								</a>
-							</Fragment>
-						);
-					})}
-				</div>
-			</div>
+  return (
+    <Fragment>
+      <div className={`lg:px-24 md:px-12 px-6 bg-secondary z-[999999] text-white py-1 ${!navOpen ? "hidden" : "flex"} justify-between items-center`}>
+        <div className="flex flex-col md:flex-row">
+          <p className="flex items-center justify-start md:justify-center text-xs mr-2">
+            <FiPhoneCall className="mr-1" /> (+234) 7043837916
+          </p>
+          <p className="flex items-center justify-start md:justify-center text-xs">
+            <SiMinutemailer className="mr-1" /> info@viridiconsultsltd.com.ng
+          </p>
+        </div>
+        <div className="flex">
+          {socials.map((item) => {
+            return (
+              <Fragment key={item.id}>
+                <a href={item.link} key={item.id} target="_blank" rel="noreferrer" className="ml-3">
+                  {item.logo}
+                </a>
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
 
-			<div className='lg:px-24 sticky z-[999990000] top-0 left-0 right-0 bg-white md:px-12 px-6 md:py-4 py-3 '>
-				<div className='flex justify-between md:items-center'>
-					<div className='flex justify-between items-center w-full md:w-1/3'>
-						{/* <p className='lg:text-4xl md:text-2xl text-xl font-bold'>Viridi <span>Consult Ltd.</span></p> */}
-						<Link to='/'>
-							<img src={Logo} alt='' className='md:w-auto w-[100px] h-auto md:h-[70px]' />
-						</Link>
-						<div>
-							<GiHamburgerMenu className='text-2xl md:hidden cursor-pointer hover:text-green-900' onClick={handleNavOpen} />
-						</div>
-					</div>
-					<nav className=' hidden md:flex xl:w-[30%] md:w-[50%]'>
-						<ul className='md:flex w-full justify-between'>
-							{links.map((items) => {
-								return (
-									<Fragment key={items.id}>
-										<li key={items.id}>
-											<Link to={items.to} className=' uppercase hover:underline hover:text-green-900 ml-2 lg:text-sm text-sm'>
-												{items.link}
-											</Link>
-										</li>
-									</Fragment>
-								);
-							})}
-						</ul>
-					</nav>
-				</div>
-				<nav className='w-full transition-all'>
-					<ul className={`overflow-hidden transition-all ${!navOpen ? "flex flex-col md:hidden h-[200px] py-6 w-full justify-between" : "h-0"}`}>
-						{links.map((items) => {
-							return (
-								<Fragment key={items.id}>
-									<li className='mb-3' key={items.id}>
-										<Link to={items.to} className=' uppercase font-bold hover:underline hover:text-green-900 text-xs' onClick={handleNavOpen}>
-											{items.link}
-										</Link>
-									</li>
-								</Fragment>
-							);
-						})}
-					</ul>
-				</nav>
-			</div>
-			<Outlet />
-		</Fragment>
-	);
+      <div className="lg:px-24 sticky z-[999990000] top-0 left-0 right-0 bg-white md:px-12 px-6 md:py-4 py-3 ">
+        <div className="flex justify-between md:items-center">
+          <div className="flex justify-between items-center w-full md:w-1/3">
+            {/* <p className='lg:text-4xl md:text-2xl text-xl font-bold'>Viridi <span>Consult Ltd.</span></p> */}
+            <Link to="/">
+              <img src={Logo} alt="" className="md:w-auto w-[100px] h-auto md:h-[70px]" />
+            </Link>
+            <div>
+              <GiHamburgerMenu className="text-2xl md:hidden cursor-pointer hover:text-green-900" onClick={handleNavOpen} />
+            </div>
+          </div>
+          <nav className=" hidden md:flex xl:w-[30%] md:w-[50%]">
+            <ul className="md:flex w-full justify-between">
+              {links.map((items) => {
+                return (
+                  <Fragment key={items.id}>
+                    <li key={items.id}>
+                      <Link to={items.to} className=" uppercase hover:underline hover:text-green-900 ml-2 lg:text-sm text-sm">
+                        {items.link}
+                      </Link>
+                    </li>
+                  </Fragment>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+        <nav className="w-full transition-all">
+          <ul className={`overflow-hidden transition-all ${!navOpen ? "flex flex-col md:hidden h-[200px] py-6 w-full justify-between" : "h-0"}`}>
+            {links.map((items) => {
+              return (
+                <Fragment key={items.id}>
+                  <li className="mb-3" key={items.id}>
+                    <Link to={items.to} className=" uppercase font-bold hover:underline hover:text-green-900 text-xs" onClick={handleNavOpen}>
+                      {items.link}
+                    </Link>
+                  </li>
+                </Fragment>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
